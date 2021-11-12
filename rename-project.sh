0332c6c16d4bc52d5f015a1cffdea499511ea10b
Tony Kay <tony.kay@gmail.com>
2021-11-11 20:13:01 -0800

    added instructions

--- rename-project.sh
+++ rename-project.sh
@@ -11,6 +11,7 @@ mkdir -p src/main/$directory
 mkdir -p src/test/$directory
 rsync -a src/main/com/example/* src/main/$directory/
 rsync -a src/test/com/example/* src/test/$directory/
+rm -rf src/main/com/example src/test/com/example
 for f in shadow-cljs.edn Makefile $(find src/dev src/main/$directory src/test/$directory -type f)
 do
   sed -i -e "s/com.example/$1/g" $f
